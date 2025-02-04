4fc1cdbbd7c5936f0b30a24fba34f881a4a7e15e
Wahid Khan <wahidzk0091@gmail.com>
2025-02-04 13:45:07 +0530

    final2
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,47 +6,48 @@ export default function Home() {
     {
       title: "Reverse Coding",
       date: "21st February 2025",
-      time: "8:00 am",
+      time: "10:00 am - 10:30 am",
       location: "IT Lab, 2nd Floor",
       description: "Test your knowledge across the tech universe. An output will be given for which the participants have to write the source code",
       rules: [
         "Individual Participation",
-        "Time limit: 90 minutes.",
+        "Time limit: 30 minutes.",
         "Use of any external resources is prohibited."
       ]
     },
     {
-      title: "Code Debug",
+      title: "Code Debugging",
       date: "21st February 2025",
-      time: "8:45 am",
+      time: "10:30 am - 11:00 am",
       location: "IT Lab, 2nd Floor",
       description: "Unravel the mysteries in buggy codes.",
       rules: [
         "Individual participation.",
-        "Time limit: 60 minutes.",
-        "Languages allowed: C, Python."
+        "Time limit: 30 minutes.",
+        "Languages allowed: C, Python, Java."
       ]
     },
     {
-      title: "Reverse Engineering: Decoding Jumbled Words",
+      title: "Crazy Words",
       date: "21st February 2025",
       time: "9:30 am",
-      location: "Signal Processing Lab, 2nd Floor",
+      location: "4th Floor",
       description: "Decipher jumbled outputs and recreate the source.",
       rules: [
         "Individual participation.",
-        "Time limit: 60 minutes.",
+        "Time limit: 15 minutes.",
       ]
     },
     {
       title: "Battle Royales!: BGMI, Free Fire",
       date: "21st February 2025",
       time: "10:00 am",
-      location: "IT Lab, 2nd Floor",
+      location: "5th Floor",
       description: "Engage in Battle Royales with your friends, competiting for the best team in this arena of players.",
       rules: [
-        "Squad matches (Max 4 players per team).",
+        "Squad matches (Only Squads allowed).",
         "Participants must bring their own devices.",
+        "No internet will be provided.",
         "Use of any type of hacks and modification tools will result in disqualification."
       ]
     }
@@ -55,15 +56,16 @@ export default function Home() {
   return (
     <div className="text-center relative z-10">
       <h1 className="text-6xl font-bold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-800 minimal-glow">
-        <br></br><br></br>Technotia
+        <br></br><br></br>T E C H N O T I A
       </h1>
       <p className="text-2xl mb-8 text-blue-300">Embark on a Cosmic Journey of Technology and Innovation</p>
       <div className="mb-12">
-        <p className="text-xl text-green-300">10th & 11th January, 2025</p>
-        <p className="text-lg text-yellow-300">Kamaladevi College of Arts and Commerce, Kalyan, Vitthalwadi (E)</p>
+        <p className="text-xl text-green-300">21st February, 2025</p>
+        <p className="text-xl text-yellow-300">Kamaladevi College of Arts and Commerce, Kalyan, Vitthalwadi (E)</p>
+        <p className="text-lg text-yellow-300">Department of CS-IT</p>
       </div>
       <Link 
-        href="https://forms.gle/WEHZMs5bdhbZywes5" 
+        href="https://forms.gle/P1BZE2qfnUQWBpjr5" 
         target="_blank" 
         rel="noopener noreferrer" 
         className="inline-block px-8 py-3 text-lg font-semibold text-white bg-purple-600 rounded-full hover:bg-purple-700 transition-colors duration-300 transform hover:scale-105"
